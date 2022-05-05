f7e0610b652e96c6329279176f6b5e836bd877d1
Rishabh-dot-tomar <70138890+Rishabh-dot-tomar@users.noreply.github.com>
2022-05-05 05:30:39 +0530

    Folder structure added, server setup completed with initial views and layouts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 8000
 const express = require('express')
 const res = require('express/lib/response')
 const path=require('path')
-
+const expressLayouts = require('express-ejs-layouts')
 
 //some other requirements
 const app = express()
@@ -20,11 +20,11 @@ app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
 
+//call express layouts before routes as it routes will be requiring to render these layouts so should be loaded before routes
+app.use(expressLayouts)
+//use express router
+app.use('/',require('./routes'))
 
-app.get('/',function(req,res){
-
-    return res.render('home')
-})
 
 //listining on port
 app.listen(PORT, (err) => {
