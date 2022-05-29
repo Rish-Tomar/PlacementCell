61b599a40f35ec5d19e1f6242457f077727d19f5
Rishabh-dot-tomar <70138890+Rishabh-dot-tomar@users.noreply.github.com>
2022-05-29 08:32:21 +0530

    mongoose topology false

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,10 +5,10 @@ const env = require('./environment')
 
 // mongoose.connect(`mongodb://localhost/PlacementCell_development`)
 
-const connection_url ='mongodb+srv://admin:rootroot@cluster0.6c9um.mongodb.net/?retryWrites=true&w=majority?directConnection=true'
+const connection_url ='mongodb+srv://admin:rootroot@cluster0.6c9um.mongodb.net/?retryWrites=true&w=majority&directConnection=true'
 mongoose.connect(connection_url,{
     useNewUrlParser:true,
-    useUnifiedTopology:true,
+    useUnifiedTopology:false,
 }).then(()=>{console.log('connected to atlas')})
 .catch((err)=> console.log('error',err))
 
